932a8e2afe5dae786f3c7488711620375dc0551a
Frank <fgsh.mcfly@gmail.com>
2023-03-06 21:07:34 -0600

    Se modifico el nombre de las imagenes y se cambio el script de cambiar la imagen del fondo

--- js/interface/background.js
+++ js/interface/background.js
@@ -1,7 +1,7 @@
 export default function background(estado, tiempo) {
 
     const bg = (clima) => {
-        document.body.style.background = `url(/img/background/${clima}.jpg)`;
+        document.documentElement.style.setProperty('--weatherBG', `url(/img/background/${clima}.jpg`);
         card_bg(clima);
     };
 
@@ -18,19 +18,20 @@ export default function background(estado, tiempo) {
     let time = tiempo.split(' ');
     let onlyHour = time[0].split(':')[0];
     time = time[1]; // a.m. y p.m.
+ 
 
     switch (estado) {
         // -- Despejado --
-        case 'Cielo claro': setBackGround(time ,onlyHour, 'dia/day', 'dia/night'); break
+        case 'Cielo claro': setBackGround(time ,onlyHour, 'dia/clear_day', 'dia/clear_night'); break;
 
         // -- Nubes --
-        case 'Nubes dispersas':setBackGround(time ,onlyHour, 'nubes/algo_nubes/day', 'nubes/algo_nubes/night'); break; 
+        case 'Nubes dispersas': setBackGround(time ,onlyHour, 'nubes/algo_nubes/something_day', 'nubes/algo_nubes/something_night');break; 
 
-        case 'Algo de nubes': setBackGround(time ,onlyHour, 'nubes/algo_nubes/day', 'nubes/algo_nubes/night'); break;  
+        case 'Algo de nubes': setBackGround(time ,onlyHour, 'nubes/algo_nubes/something_day', 'nubes/algo_nubes/something_night'); break;  
 
-        case 'Nubes': setBackGround(time ,onlyHour, 'nubes/nubes/clouds_day', 'nubes/nubes/night'); break
+        case 'Nubes': setBackGround(time ,onlyHour, 'nubes/nubes/clouds_day', 'nubes/nubes/clouds_night'); break
         ;
-        case 'Muy nuboso':  setBackGround(time ,onlyHour, 'nubes/muy_nubuso/day', 'nubes/muy_nubuso/night'); break;
+        case 'Muy nuboso':  setBackGround(time ,onlyHour, 'nubes/muy_nubuso/heavy_cloud_day', 'nubes/muy_nubuso/heavy_cloud_night'); break;
 
         // -- Lluvia --
         case 'Lluvia ligera': setBackGround(time ,onlyHour, 'lluvia/lluvia_ligera/day', 'lluvia/lluvia_ligera/night'); break;
@@ -38,10 +39,12 @@ export default function background(estado, tiempo) {
         case 'Lluvia moderada': setBackGround(time ,onlyHour, 'lluvia/lluvia_moderada/day', 'lluvia/lluvia_moderada/night'); break;
 
         // -- Niebla --
-        case 'Niebla': setBackGround(time ,onlyHour, 'niebla/niebla', 'niebla/niebla'); break;
+        case 'Niebla': setBackGround(time ,onlyHour, 'niebla/fog', 'niebla/fog'); break;
 
         // -- Nevada --
-        case 'Nevada ligera': setBackGround(time ,onlyHour, 'snowfall/nevada_ligera/day', 'snowfall/nevada_ligera/night'); break;
+        case 'Nevada ligera': setBackGround(time ,onlyHour, 'snowfall/nevada_ligera/snow_day', 'snowfall/nevada_ligera/snow_night'); break;
+
+        case 'Lluvia de gran intensidad': setBackGround(time ,onlyHour, 'lluvia/Heavy_rain/heavy_rain', 'lluvia/Heavy_rain/heavy_rain'); break;
 
         default:
             setBackGround(time ,onlyHour, 'day', 'night'); break;
